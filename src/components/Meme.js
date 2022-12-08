e21f98c4208ef1cdadd5c39f5987e21f977e0806
bet_h <bettybhan@gmail.com>
2022-12-08 17:11:20 -0500

    Set state for Meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,10 +2,13 @@ import React from "react"
 import memesData from "../memesData.js"
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage]= React.useState("")
+
     function newMeme() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        setMemeImage(memesArray[randomNumber].url)
     }
     return (
         <main>
@@ -14,6 +17,7 @@ export default function Meme() {
                 <input type="text" className="form-input" placeholder="Bottom Text"></input>
                 <button className="form-button" onClick={newMeme}>Get a new meme image 🖼</button>
             </div>
+            <img src={memeImage} className="meme-image"/>
         </main>
     )
 }
