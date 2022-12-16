b81cbf8166a52c82598313f50c20636f66c7a620
bet_h <bettybhan@gmail.com>
2022-12-15 22:21:16 -0500

    Update state on Meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,21 +4,31 @@ import memesData from "../memesData.js"
 export default function Meme() {
 
     // state couldn't be left as empty string - will return error so placeholder img was put in
-    const [memeImage, setMemeImage]= React.useState("http://i.imgflip.com/1bij.jpg")
+    const [meme, setMeme]= React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
 
-    function newMeme() {
-        const memesArray = memesData.data.memes
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
     return (
         <main>
             <div className="form">
                 <input type="text" className="form-input" placeholder="Top Text"></input>
                 <input type="text" className="form-input" placeholder="Bottom Text"></input>
-                <button className="form-button" onClick={newMeme}>Get a new meme image 🖼</button>
+                <button className="form-button" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
-            <img src={memeImage} className="meme-image"/>
+            <img src={meme.randomImage} className="meme-image"/>
         </main>
     )
 }
