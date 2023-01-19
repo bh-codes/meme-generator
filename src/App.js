b2ebcc13dfea8249651bd52c088bfdcf52bf2573
Betty H <101302743+bh-codes@users.noreply.github.com>
2023-01-19 16:34:11 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Header from "./components/Header.js";
+import Meme from "./components/Meme.js";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        className="header--image"
+        src={require("./troll-face.png")}
+        alt="Troll face"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
+}
